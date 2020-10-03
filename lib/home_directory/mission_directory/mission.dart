6acb46d996e7b6d87a5e07dd19da03d17e419923
shoyab01 <shaikshoyab01@gmail.com>
2020-10-03 18:43:14 +0530

    mission integrity appbar

--- lib/home_directory/mission_directory/mission.dart
+++ lib/home_directory/mission_directory/mission.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:terra_transcender/ThemeData/fontstyle.dart';
 
 class Mission extends StatefulWidget {
   @override
@@ -10,7 +11,19 @@ class _MissionState extends State<Mission> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text("Mission Integrity", style: Font_Style()
+            .montserrat_SemiBold(Font_Style.secondaryLightColor, 22.0),),
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Icon(Icons.arrow_back_ios, size: 24.0,
+            color: Font_Style.secondaryLightColor,),
+        ),
+      ),
     );
   }
 }
